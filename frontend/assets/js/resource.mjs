b12876651056badd4ead4e4351724aaa047d70b5
Moderocky <admin@moderocky.com>
2022-05-15 10:42:48 +0100

    Tidy code.

--- frontend/assets/js/resource.mjs
+++ frontend/assets/js/resource.mjs
@@ -141,16 +141,13 @@ async function countDownloads() {
     for (let release of releases) {
         if (release == null || release.assets == null) continue;
         downloads += (release.assets.length > 0) ? release.assets[0].download_count : 0;
-        for (const element of document.querySelectorAll('[data-downloads-count]')) {
-            element.innerText = abbreviateNumber(downloads);
-        }
+        for (const element of document.querySelectorAll('[data-downloads-count]')) element.innerText = abbreviateNumber(downloads);
     }
 }
 function abbreviateNumber(value) {
     let altered = value;
     if (value >= 1000) {
-        const suffixes = ["", "k", "m", "b", "t"];
-        const suffix = Math.floor(("" + value).length / 3);
+        const suffixes = ["", "k", "m", "b", "t"], suffix = Math.floor(("" + value).length / 3);
         let short = '';
         for (let precision = 2; precision >= 1; precision--) {
             short = parseFloat((suffix !== 0 ? (value / Math.pow(1000, suffix)) : value).toPrecision(precision));
