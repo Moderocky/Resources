9c0479d543b48009a3f9ea107b472b41ca2584ae
Moderocky <admin@moderocky.com>
2023-04-09 10:56:30 +0100

    Improve statistics box appearance.

--- frontend/assets/js/blocks/statistics.mjs
+++ frontend/assets/js/blocks/statistics.mjs
@@ -1,26 +1,23 @@
 import {dom} from "../lib/dom.mjs";
+
 export const statistics = (data) => dom.create(`
-        <div class="w-full h-full p-2 transition-opacity">
-            <div class="p-6 h-full rounded-xl group sm:flex space-x-2 bg-slate-900 shadow-xl">
-                <div class="space-y-4 flex flex-col h-full text-gray-300 text-lg">
-                    <h3 class="text-xl">This Week</h3>
-                    <table>
-                        <tr>
-                            <td class="text-emerald-300 pr-2">{pushes}</td><td>Changes</td>
-                        </tr>
-                        <tr>
-                            <td class="text-emerald-300 pr-2">{prs}</td><td>Pull Requests</td>
-                        </tr>
-                        <tr>
-                            <td class="text-emerald-300 pr-2">{forks}</td><td>Forks Created</td>
-                        </tr>
-                        <tr>
-                            <td class="text-emerald-300 pr-2">{issues}</td><td>Issues Created</td>
-                        </tr>
-                        <tr>
-                            <td class="text-emerald-300 pr-2">{reviews}</td><td>Code Reviews</td>
-                        </tr>
-                    </table>
+        <div class="w-full p-2 opacity-0 transition-opacity">
+            <div class="py-6 px-4 m-0 space-y-2 rounded-xl rounded-xl bg-slate-900 dark:bg-white dark:bg-opacity-10 bg-opacity-10 shadow-xl">
+                <h5 class="text-xl mb-6 text-gray-600 dark:text-gray-100 text-center">This Month</h5>
+                <div class="w-full p-2.5 space-x-2 rounded-lg text-white bg-gradient-to-r from-indigo-500 to-violet-400">
+                    <i class="fa-solid fa-up-long w-4"></i> ${data.pushes} Commits
+                </div>
+                <div class="w-full p-2.5 space-x-2 rounded-lg text-white bg-gradient-to-r from-sky-500 to-cyan-400">
+                    <i class="fa-solid fa-code-pull-request w-4"></i> ${data.prs} Pull Requests
+                </div>
+                <div class="w-full p-2.5 space-x-2 rounded-lg text-white bg-gradient-to-r from-green-500 to-emerald-400">
+                    <i class="fa-solid fa-code-fork w-4"></i> ${data.forks} Forks Created
+                </div>
+                <div class="w-full p-2.5 space-x-2 rounded-lg text-white bg-gradient-to-r from-orange-500 to-amber-400">
+                    <i class="fa-solid fa-triangle-exclamation w-4"></i> ${data.issues} Issues Reported
+                </div>
+                <div class="w-full p-2.5 space-x-2 rounded-lg text-white bg-gradient-to-r from-rose-500 to-red-400">
+                    <i class="fa-solid fa-magnifying-glass w-4"></i> ${data.reviews} Code Reviews
                 </div>
             </div>
         </div>`, data);
